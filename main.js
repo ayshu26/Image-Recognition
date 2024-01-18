35cdaa40478ad6ea8e6a2d1fe199bfade49694ea
ayshu26 <126812520+ayshu26@users.noreply.github.com>
2024-01-18 17:47:53 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function capture(){
-
+Webcam.snap (function(picture){
+    document.getElementById("result").innerHTML="<img id='pic' src='"+picture+"'>"
+})
 }
 function check(){
     
+}
+Webcam.set({width:350,height:300,image_format:"png",png_quality:90})
+camera=document.getElementById("camera")
+Webcam.attach("#camera")
+console.log(ml5.version)
+imageclassifier=ml5.imageClassifier("",modelloaded)
+function modelloaded(){
+    console.log("model is ready");
 }
