f8bc0dbd1f2c06ee773c138501f035d23c21189f
ayshu26 <126812520+ayshu26@users.noreply.github.com>
2024-01-30 17:56:07 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ Webcam.snap (function(picture){
 })
 }
 function check(){
-    
+    img = document.getElementById("pic")
+    imgclassifier.classify(img, gotresult)
+  
 }
 Webcam.set({width:350,height:300,image_format:"png",png_quality:90})
 camera=document.getElementById("camera")
 Webcam.attach("#camera")
 console.log(ml5.version)
-imageclassifier=ml5.imageClassifier("",modelloaded)
+imgclassifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Ue9tmY13f/model.json",modelloaded)
 function modelloaded(){
     console.log("model is ready");
+}
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("resultObject").innerHTML=results[0].label
+        document.getElementById("resultAccuracy").innerHTML=results[0].confidence.toFixed(3)
+    }
 }
